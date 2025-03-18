9b37101c8795b43018f6e79d8568ab356f420a6c
lautaro perez <lauttiiperez@gmail.com>
2025-03-18 15:12:12 -0300

    tablero de tateti con su reset game

---
+++ src/App.jsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+const Square = ({ children, updateBoard, isSelectd, index }) => {
+  const className = `square ${isSelectd ? "is-selected" : ""}`;
+
+  const handleClick = () => {
+    updateBoard(index);
+  };
+
+  return (
+    <div className={className} onClick={handleClick}>
+      {children}
+    </div>
+  );
+};
+
+const combosGanadores = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [5, 6, 7],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const [turn, setTurn] = useState(TURNS.X);
+
+  const [winner, setWinner] = useState(null);
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
+  };
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of combosGanadores) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+
+    return null;
+  };
+
+  const updateBoard = (index) => {
+    if (board[index] || winner) return;
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    const newTurn = turn == TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
+  };
+
+  return (
+    <main className="board">
+      <h1>TaTeTi</h1>
+      <button onClick={resetGame}>Resetear Juego</button>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+
+      <section className="turn">
+        <Square isSelectd={turn == TURNS.X}>{TURNS.X}</Square>
+        <Square isSelectd={turn == TURNS.O}>{TURNS.O}</Square>
+      </section>
+
+      {winner !== null && (
+        <section className="winner">
+          <div className="text">
+            <h2>{winner === false ? "Empate" : "Gano:"}</h2>
+
+            <header className="win">
+              {winner && <Square>{winner}</Square>}
+            </header>
+
+            <footer>
+              <button onClick={resetGame}>Empezar de nuevo</button>
+            </footer>
+          </div>
+        </section>
+      )}
+    </main>
+  );
+}
+
+export default App;
